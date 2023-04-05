2eb75c0e822bceb7e9330372f0d7a44f72cc3f1c
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2023-04-05 16:32:57 +0300

    Made a post api

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const app = express()
 const multer = require('multer')
+const parser = require('body-parser')
+const cors = require('cors')
 const server = require('mongoose')
 
-server.connect('mongodb://127.0.0.1:27017/furnituredb')
+app.use(cors())
+
+const userSchema = server.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = server.model("User", userSchema)
+
+server.connect('mongodb://127.0.0.1:27017/furniture_store')
 .then(() => {
-    app.listen(3000)
+    app.listen(2000, () => {
+        console.log("Listening to port 2000")
+    })
 })
 .catch(err => {
-    console.log(err.message)
+    console.log("Error" + err.message)
+})
+
+
+app.post('/login-data', parser.json(), (req, res) => {
+    console.log("Request made")
+    let name = req.body.userName
+    let email = req.body.email
+    let password = req.body.password
+    const user = new User({
+        name: name,
+        email: email,
+        password: password
+    })
+    user.save()
+    .then((userObject) => {
+        res.json(userObject)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+    // res.redirect('/home')
 })
